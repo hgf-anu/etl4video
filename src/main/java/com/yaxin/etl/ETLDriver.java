3a82fa3d89e0a240afb2f99455890a8f665b373b
hgf-anu <956246882@qq.com>
2019-10-19 19:51:18 +0800

    simple etl

---
+++ src/main/java/com/yaxin/etl/ETLDriver.java
@@ -0,0 +1,35 @@
+package com.yaxin.etl;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import javax.xml.soap.Text;
+import java.io.IOException;
+
+public class ETLDriver {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Job job = Job.getInstance(new Configuration());
+
+        job.setJarByClass(ETLDriver.class);
+
+        job.setMapperClass(ETLMapper.class);
+
+        //因为没有reduce的操作,所有设置为0
+        job.setNumReduceTasks(0);
+
+        job.setMapOutputKeyClass(Text.class);
+
+        job.setOutputValueClass(NullWritable.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        boolean b = job.waitForCompletion(true);
+
+        System.exit(b ? 0 : 1);
+    }
+}
